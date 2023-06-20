3b703feffeba5ed7f9365775b3f1db0316c5d220
Susk4 <35198313+Susk4@users.noreply.github.com>
2023-06-20 22:15:44 +0200

    init vue

---
+++ airlines-ui/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div v-for="user in users" :key="user.id">
+    {{ user.name }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      users: [],
+    };
+  },
+  mounted() {
+    fetch("http://127.0.0.1:8000/api/users")
+      .then((response) => response.json())
+      .then((data) => {
+        this.users = data;
+      });
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
